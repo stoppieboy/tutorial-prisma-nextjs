4dd494c6902608933aec4a91869a30ec182f4d1c
Shivam <shivamv1332@gmail.com>
2023-09-20 00:54:52 +0530

    tweeks

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-import Post from './components/post';
+import Post from '@/app/components/Post';
 import styles from './page.module.css'
 import prisma from '@/lib/prisma'
 import Link from 'next/link';
